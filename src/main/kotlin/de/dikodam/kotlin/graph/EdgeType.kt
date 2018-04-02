7af66fe908bb5c2c7ff94e42ff7eb6ccb23b8c9a
adiko <dikodam@googlemail.com>
2018-04-02 23:01:17 +0200

    edgetype implemented

--- src/main/kotlin/de/dikodam/kotlin/graph/EdgeType.kt
+++ src/main/kotlin/de/dikodam/kotlin/graph/EdgeType.kt
@@ -1,4 +1,15 @@
 package de.dikodam.kotlin.graph
 
 enum class EdgeType {
+    TAXI, BUS, METRO, SHIP;
+
+    fun getByCharacter(char: String): EdgeType {
+        return when (char) {
+            "t" -> TAXI
+            "b" -> BUS
+            "m" -> METRO
+            "s" -> SHIP
+            else -> throw IllegalArgumentException(char)
+        }
+    }
 }
