04e3b280441bdbd3997072f51411951b4e8bedf0
adiko <dikodam@googlemail.com>
2018-04-04 10:55:44 +0200

    graph impl get neighbor

--- src/main/kotlin/de/dikodam/kotlin/api/IGraph.kt
+++ src/main/kotlin/de/dikodam/kotlin/api/IGraph.kt
@@ -1,10 +1,9 @@
 package de.dikodam.kotlin.api
 
-import de.dikodam.kotlin.graph.Vertex
-
 interface IGraph {
-    fun getVerticesList(): List<Vertex>
-    fun getVerticesMap(): Map<Int, Vertex>
-    fun getVerticesAfterMoveSequence(start: Vertex, moves: List<Move>): Set<Vertex>
+    val vertices: Map<Int, IVertex>
+    fun getNeighbors(vertex: IVertex): Set<IVertex>
+    fun getNeighborsWithMove(vertex: IVertex, move: Move) : Set<IVertex>
+    fun getVerticesAfterMoveSequence(start: IVertex, moves: List<Move>): Set<IVertex>
 
 }
--- src/main/kotlin/de/dikodam/kotlin/graph/Graph.kt
+++ src/main/kotlin/de/dikodam/kotlin/graph/Graph.kt
@@ -1,5 +1,29 @@
 package de.dikodam.kotlin.graph
 
-class Graph {
+import de.dikodam.kotlin.api.IEdge
+import de.dikodam.kotlin.api.IGraph
+import de.dikodam.kotlin.api.IVertex
+import de.dikodam.kotlin.api.Move
+
+class Graph(override val vertices: Map<Int, IVertex>, val edges: Set<IEdge>) : IGraph {
+
+    override fun getNeighbors(vertex: IVertex): Set<IVertex> {
+        return vertex.edges
+            .map { edge -> edge.getOtherVertex(vertex) }
+            .toSet()
+    }
+
+    override fun getNeighborsWithMove(vertex: IVertex, move: Move): Set<IVertex> {
+        return vertex.edges
+            // TODO filter by move
+            .filter { edge -> true }
+            .map { edge -> edge.getOtherVertex(vertex) }
+            .toSet()
+    }
+
+    override fun getVerticesAfterMoveSequence(start: IVertex, moves: List<Move>): Set<IVertex> {
+        TODO("not implemented")
+    }
+
 
 }
