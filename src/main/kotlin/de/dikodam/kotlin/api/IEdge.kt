d29cc8c2f4246117271009d988eca1f3faf73d66
adiko <dikodam@googlemail.com>
2018-04-04 10:36:34 +0200

    Edge interface + impl

--- src/main/kotlin/de/dikodam/kotlin/api/IEdge.kt
+++ src/main/kotlin/de/dikodam/kotlin/api/IEdge.kt
@@ -1,6 +1,6 @@
 package de.dikodam.kotlin.api
 
 interface IEdge {
-    fun getVertices()
-    fun getOtherVertexThan()
+    val vertices: Set<IVertex>
+    fun getOtherVertex(notThis: IVertex): IVertex
 }
--- src/main/kotlin/de/dikodam/kotlin/graph/Edge.kt
+++ src/main/kotlin/de/dikodam/kotlin/graph/Edge.kt
@@ -1,5 +1,18 @@
 package de.dikodam.kotlin.graph
 
 import de.dikodam.kotlin.api.EdgeType
+import de.dikodam.kotlin.api.IEdge
+import de.dikodam.kotlin.api.IVertex
+import java.lang.IllegalArgumentException
 
-data class Edge(val type: EdgeType, val vertex1: Vertex, val vertex2: Vertex)
+data class Edge(val type: EdgeType, private val vertex1: IVertex, private val vertex2: IVertex) : IEdge {
+    override val vertices: Set<IVertex> = setOf(vertex1, vertex2)
+
+    override fun getOtherVertex(notThis: IVertex): IVertex =
+        when (notThis) {
+            vertex1 -> vertex2
+            vertex2 -> vertex1
+            else -> throw IllegalArgumentException("Edge doesn't reference the given vertex $notThis")
+        }
+
+}
