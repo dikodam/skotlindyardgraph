961167bbe1309f39e9c34221f22a2c4309d81a47
Adam <dikodam@googlemail.com>
2018-04-05 20:56:41 +0200

    fixed default inputFile

--- src/main/java/de/dikodam/skotlindyard/Main.java
+++ src/main/java/de/dikodam/skotlindyard/Main.java
@@ -7,5 +7,8 @@ import de.dikodam.skotlindyard.graphbuilding.ParserKt;
 public class Main {
     public static void main(String[] args) {
         Graph graph = GraphBuilderKt.buildGraph(ParserKt.getDefaultFile());
+
+        graph.getNeighborsOf(199)
+            .forEach(System.out::println);
     }
 }
