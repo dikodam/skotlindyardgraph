1f28d7cb64b755480c011a28aadcfb3b37eb4118
Adam <dikodam@googlemail.com>
2018-04-05 20:28:06 +0200

    cleanup

--- src/main/kotlin/de/dikodam/skotlindyard/graphbuilding/GraphBuilder.kt
+++ src/main/kotlin/de/dikodam/skotlindyard/graphbuilding/GraphBuilder.kt
@@ -1,6 +1,8 @@
 package de.dikodam.skotlindyard.graphbuilding
 
 import de.dikodam.skotlindyard.api.EdgeType
+import de.dikodam.skotlindyard.api.IEdge
+import de.dikodam.skotlindyard.api.IVertex
 import de.dikodam.skotlindyard.graph.Edge
 import de.dikodam.skotlindyard.graph.Graph
 import de.dikodam.skotlindyard.graph.Vertex
@@ -9,8 +11,7 @@ import java.io.File
 typealias EdgeInfo = Triple<EdgeType, Int, Int>
 
 fun buildGraph(file: File): Graph {
-    val edgeInfo = readLinesFromFile(file)
-        .map { extractEdgeInfoFromLine(it) }
+    val edgeInfo = readLinesFromFile(file).map { extractEdgeInfoFromLine(it) }
     return buildGraph(edgeInfo)
 }
 
@@ -20,13 +21,12 @@ fun buildGraph(edgeInfo: Collection<EdgeInfo>): Graph {
     return Graph(vertices, edges)
 }
 
-fun extractVertices(edgeInfos: Collection<EdgeInfo>): Map<Int, Vertex> {
+fun extractVertices(edgeInfos: Collection<EdgeInfo>): Map<Int, IVertex> {
     return edgeInfos.flatMap { listOf(it.second, it.third) }
         .distinct()
         .map { vertexID -> Pair(vertexID, Vertex(vertexID)) }
         .toMap()
 }
 
-
-fun buildEdges(edgeInfos: Collection<EdgeInfo>, vertices: Map<Int, Vertex>): List<Edge> =
+fun buildEdges(edgeInfos: Collection<EdgeInfo>, vertices: Map<Int, IVertex>): List<IEdge> =
     edgeInfos.map { Edge(it.first, vertices.getValue(it.second), vertices.getValue(it.third)) }
--- src/main/kotlin/de/dikodam/skotlindyard/graphbuilding/Parser.kt
+++ src/main/kotlin/de/dikodam/skotlindyard/graphbuilding/Parser.kt
@@ -5,12 +5,13 @@ import java.io.File
 import java.util.stream.Collectors
 
 
-fun readLinesFromFile(file: File): List<String> =
-    file.bufferedReader()
+fun readLinesFromFile(file: File): List<String> {
+    return file.bufferedReader()
         .lines()
         .collect(Collectors.toList())
+}
 
-fun extractEdgeInfoFromLine(line: String): EdgeInfo =
+fun extractEdgeInfoFromLine(line: String): EdgeInfo {
     EdgeType.values()
         .filter { edgeType -> line.contains(edgeType.singleChar) }
         .map {
@@ -18,9 +19,9 @@ fun extractEdgeInfoFromLine(line: String): EdgeInfo =
             return EdgeInfo(it, vertices[0], vertices[1])
         }
         .first()
+}
 
-
-fun defaultFile() {
-    val adress = Thread.currentThread().contextClassLoader.getResource("scotlandYardGraph").file
-    val file = File(adress)
+fun getDefaultFile(): File {
+    val fileURL = Thread.currentThread().contextClassLoader.getResource("scotlandYardGraph")
+    return File(fileURL.file)
 }
