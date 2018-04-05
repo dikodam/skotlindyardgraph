04e437c22e62ca9553afb613076d05a43c432cd9
Adam <dikodam@googlemail.com>
2018-04-05 20:39:13 +0200

    Main.java added, but something is broken...

---
+++ src/main/java/de/dikodam/skotlindyard/Main.java
@@ -0,0 +1,11 @@
+package de.dikodam.skotlindyard;
+
+import de.dikodam.skotlindyard.graph.Graph;
+import de.dikodam.skotlindyard.graphbuilding.GraphBuilderKt;
+import de.dikodam.skotlindyard.graphbuilding.ParserKt;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = GraphBuilderKt.buildGraph(ParserKt.getDefaultFile());
+    }
+}
--- src/main/kotlin/de/dikodam/skotlindyard/Main.kt
+++ src/main/kotlin/de/dikodam/skotlindyard/Main.kt
@@ -1,10 +1,12 @@
 package de.dikodam.skotlindyard
 
+import de.dikodam.skotlindyard.graphbuilding.buildGraph
+import de.dikodam.skotlindyard.graphbuilding.getDefaultFile
+
 fun main(args: Array<String>) {
-    val graph = buildGraph()
+    val graph = buildGraph(getDefaultFile())
 
     graph.getNeighborsOf(199)
         .forEach { println(it) }
-
 }
 
