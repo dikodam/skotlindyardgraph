ba7951c710c7d00ac03ed89e792b7f2c5f224b46
Adam <dikodam@googlemail.com>
2018-04-05 21:08:07 +0200

    update to IGraph interface

--- src/main/kotlin/de/dikodam/skotlindyard/api/IGraph.kt
+++ src/main/kotlin/de/dikodam/skotlindyard/api/IGraph.kt
@@ -3,9 +3,11 @@ package de.dikodam.skotlindyard.api
 interface IGraph {
     val vertices: Map<Int, IVertex>
     val edges: Set<IEdge>
-    fun getNeighborsOf(vertex: IVertex): Set<IVertex>
     fun getNeighborsOf(vertexID: Int): Set<IVertex>
+    fun getNeighborsOf(vertex: IVertex): Set<IVertex>
+    fun getNeighborsAfterMove(vertexID: Int, move: Move): Set<IVertex>
     fun getNeighborsAfterMove(vertex: IVertex, move: Move): Set<IVertex>
+    fun getVerticesAfterMoveSequence(startVertex: Int, moves: List<Move>): Set<IVertex>
     fun getVerticesAfterMoveSequence(startVertex: IVertex, moves: List<Move>): Set<IVertex>
     fun vertexAt(id: Int): IVertex
 
--- src/main/kotlin/de/dikodam/skotlindyard/graph/Graph.kt
+++ src/main/kotlin/de/dikodam/skotlindyard/graph/Graph.kt
@@ -4,15 +4,22 @@ import de.dikodam.skotlindyard.api.*
 import de.dikodam.skotlindyard.api.Move.*
 
 class Graph(override val vertices: Map<Int, IVertex>, override val edges: Set<IEdge>) : IGraph {
+    override fun getNeighborsAfterMove(vertexID: Int, move: Move): Set<IVertex> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getVerticesAfterMoveSequence(startVertex: Int, moves: List<Move>): Set<IVertex> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getNeighborsOf(vertexID: Int): Set<IVertex> =
+        getNeighborsOf(vertices.getValue(vertexID))
 
     override fun getNeighborsOf(vertex: IVertex): Set<IVertex> {
         return vertex.getAllEdges()
             .map { edge -> edge.getOtherVertex(vertex) }
             .toSet()
     }
-
-    override fun getNeighborsOf(vertexID: Int): Set<IVertex> =
-        getNeighborsOf(vertices.getValue(vertexID))
 
     override fun getNeighborsAfterMove(vertex: IVertex, move: Move): Set<IVertex> {
         return vertex.getAllEdges()
@@ -32,6 +39,11 @@ class Graph(override val vertices: Map<Int, IVertex>, override val edges: Set<IE
     override fun vertexAt(id: Int): IVertex =
         vertices.getValue(id)
 
+    override fun toString(): String {
+        // TODO
+        return super.toString()
+    }
+
     private fun IEdge.isAllowedByMove(move: Move): Boolean {
         return when (move) {
             TAXI -> this.type == EdgeType.TAXI
@@ -39,10 +51,6 @@ class Graph(override val vertices: Map<Int, IVertex>, override val edges: Set<IE
             UNDERGROUND -> this.type == EdgeType.UNDERGROUND
             BLACK -> true
         }
-    }
-
-    override fun toString(): String {
-        return super.toString()
     }
 
 }
