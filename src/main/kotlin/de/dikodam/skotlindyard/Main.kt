e1c117bd5aa06a39ebba56e53202c5b46f2be4a5
Adam <dikodam@googlemail.com>
2018-04-06 21:05:45 +0200

    example call

--- src/main/kotlin/de/dikodam/skotlindyard/Main.kt
+++ src/main/kotlin/de/dikodam/skotlindyard/Main.kt
@@ -5,7 +5,9 @@ import de.dikodam.skotlindyard.graphbuilding.extractEdgeInfo
 import de.dikodam.skotlindyard.graphbuilding.getDefaultFile
 
 fun main(args: Array<String>) {
-    val graph = buildGraph(extractEdgeInfo(getDefaultFile()))
+    val file = getDefaultFile()
+    val edgeInfo = extractEdgeInfo(file)
+    val graph = buildGraph(edgeInfo)
     graph.getNeighborsOf(199)
         .forEach { println(it) }
 }
