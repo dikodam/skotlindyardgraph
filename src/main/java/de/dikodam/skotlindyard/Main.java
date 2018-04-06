3df63c0436277e70b80466dcab5fd0e97b562524
Adam <dikodam@googlemail.com>
2018-04-06 21:11:54 +0200

    Main.java does the same as Main.kt

--- src/main/java/de/dikodam/skotlindyard/Main.java
+++ src/main/java/de/dikodam/skotlindyard/Main.java
@@ -6,11 +6,13 @@ import de.dikodam.skotlindyard.graphbuilding.GraphBuilderKt;
 import de.dikodam.skotlindyard.graphbuilding.ParserKt;
 import kotlin.Triple;
 
+import java.io.File;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Triple<EdgeType, Integer, Integer>> edgeInfo = ParserKt.extractEdgeInfo(ParserKt.getDefaultFile());
+        File file = ParserKt.getDefaultFile();
+        List<Triple<EdgeType, Integer, Integer>> edgeInfo = ParserKt.extractEdgeInfo(file);
         Graph graph = GraphBuilderKt.buildGraph(edgeInfo);
         graph.getNeighborsOf(199)
             .forEach(System.out::println);
